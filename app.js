6332cc6d1e881800bf715b901612f59ba86ebfab
Rohan Mathur <rohan@rmathur.com>
2014-12-26 22:20:49 -0500

    Clean up a bunch of unused APIs
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ var connectAssets = require('connect-assets');
 
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
-var contactController = require('./controllers/contact');
 var challengesController = require('./controllers/challenges');
 
 /**
@@ -119,8 +118,6 @@ app.get('/reset/:token', userController.getReset);
 app.post('/reset/:token', userController.postReset);
 app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
-app.get('/contact', contactController.getContact);
-app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
