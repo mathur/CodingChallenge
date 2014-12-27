fa0f9733385fba2ecf60488ef2ad01330056f9e2
Rohan Mathur <rohan@rmathur.com>
2014-12-27 10:57:57 -0500

    Add example for adding a new challenge page
    Follow this general procedure if you want to add a new challenge.
    
    Naming convention will have to be updated once more categories and stuff
    emerge, but will handle that once a more clear picture of what kind of
    challenges will be availible to the user.
    
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- controllers/challenges.js
+++ controllers/challenges.js
@@ -8,3 +8,14 @@ exports.getChallenges = function(req, res) {
     title: 'Challenges'
   });
 };
+
+/**
+ * GET /challenges/1
+ * Challenge 1 page.
+ */
+
+exports.getChallenge1 = function(req, res) {
+  res.render('challenges/1', {
+    title: 'Challenge #1'
+  });
+};
